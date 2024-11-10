ebddeef4182eee36726c17bb6285bbfb81755c0a
David Gallmeier <dg@xounges.net>
2024-11-09 21:53:28 -0500

    update gha

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -32,8 +32,8 @@ K40  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K41 \
 
 #define MIRYOKU_LAYERMAPPING_SYM( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, K42, \
-     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, K43, \
-     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, K44, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K38, K42 \
