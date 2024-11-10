954d22cf00cb3a859bce677a962edd1d7b9b6689
David Gallmeier <dg@xounges.net>
2024-11-09 22:11:19 -0500

    update gha

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,13 +5,6 @@
 
 #define XXX &none
 
-#define MIRYOKU_LAYER_SYM \
-&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT, &kp A           \
-&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
-U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
-
-
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
@@ -31,14 +24,14 @@ K40  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K41 \
 #define K41 &kp RSHFT
 
 #define MIRYOKU_LAYERMAPPING_SYM( \
-     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, K42, \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K38, \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K39, \
-K40  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K41, \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  K42, \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  K43, \
+K40  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K44, \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
